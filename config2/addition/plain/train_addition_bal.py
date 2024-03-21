b12fb9eab7f7efe29496e4031846641e1a9ebc80
ziyuwang <wangziyu@seas.upenn.edu>
2024-03-21 01:39:27 -0400

    add nocarry data

--- config2/addition/plain/train_addition_bal.py
+++ config2/addition/plain/train_addition_bal.py
@@ -1,28 +1,28 @@
 # train plain addition on NanoGPT
 
 # ===== Evaluation and Checkpointing ===== #
-out_dir = 'out2/addition_plain'
-eval_interval = 250 
+out_dir = "out2/addition_plain"
+eval_interval = 250
 eval_iters = 200
-log_interval = 10 
+log_interval = 10
 always_save_checkpoint = False
 
 # ===== Wandb logging ===== #
-wandb_log = True # override via command line if you like
-wandb_project = 'addition'
-wandb_run_name = 'addition_plain'
+wandb_log = True  # override via command line if you like
+wandb_project = "addition"
+wandb_run_name = "addition_plain"
 
 # ===== Dataset ===== #
-data_type='text'
-data_format='plain'
-operator='+'
-dataset = 'bal'
+data_type = "text"
+data_format = "plain"
+operator = "+"
+dataset = "cis700_addition"
 batch_size = 256
-block_size = 256 # context of up to 256 previous characters
-train_data_path = 'train_3digit_10000.txt'
-ckpt_path_name = 'ckpt_10000.pt'
+block_size = 256  # context of up to 256 previous characters
+train_data_path = "train_add_nocarry_2000.txt"
+ckpt_path_name = "ckpt_10000.pt"
 eval_addition = True
-start = "FILE:data/bal/test_10000.txt"
+start = "FILE:data/cis700_addition/test_add_nocarry_10000.txt"
 eval_addition_train = True
 
 # ===== NanoGPT model configuration ===== #
@@ -34,9 +34,9 @@ dropout = 0.2
 # ===== Learning Rate Policy ===== #
 learning_rate = 1e-3
 max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+lr_decay_iters = 5000  # make equal to max_iters usually
 beta2 = 0.99
 warmup_iters = 100
 
 # ===== Device ===== #
-device='cuda:0'
+device = "cuda:0"
